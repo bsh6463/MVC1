147767a58f6c8a608b84e835fc19dfccf90d6411
bsh6463 <bsh9510@gmail.com>
2021-09-09 23:52:49 +0900

    210909 WebApplication with MVC

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Controller
+ */
+@WebServlet(name = "MvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+
+        //controller -> view 이동
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        //제어건원 dispatcher에 넘김. request, response객체 같이 넘겨줌.
+        dispatcher.forward(request, response); //이동!
+
+    }
+}
